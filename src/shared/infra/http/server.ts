71aa70b69784807916164a7a9b099e99fb67ec3c
Tanguy Roberjot <tanguy.roberjot@eticasolucoes.com.br>
2020-09-21 15:28:28 -0300

    organizing project

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-
-import routes from './routes';
-import './database/index';
+import cors from 'cors';
+import '@shared/infra/typeorm';
 import 'reflect-metadata';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
