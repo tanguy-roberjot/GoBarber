d8e1a17069c5f867502da51f6601cd49540ece25
Tanguy Roberjot <tanguy.roberjot@eticasolucoes.com.br>
2020-09-24 18:17:07 -0300

    listing providers

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,9 +1,11 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '../dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
   findByEmail(email: string): Promise<User | undefined>;
   findById(id: string): Promise<User | undefined>;
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   create(data: ICreateUserDTO): Promise<User>;
   save(user: User): Promise<User>;
 }
--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -3,6 +3,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
@@ -22,6 +23,18 @@ class FakeUsersRepository implements IUsersRepository {
     this.users[findIndex] = user;
 
     return user;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
